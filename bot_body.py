27658f746e4afffd870644715c394b97f12fb2ac
BetaSugar <98915560+BetaSugar@users.noreply.github.com>
2022-09-26 20:13:35 +0300

    discord.ext integration

--- bot_body.py
+++ bot_body.py
@@ -4,7 +4,7 @@ import datetime
 import discord
 from discord.ext import commands
 import asyncio
-import os
+
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -20,71 +20,92 @@ except:
     print('The database has already been created')
 
 
-def createConfig(path: str):
+def create_config(path):
+
     # Create a config file
+
     config = configparser.ConfigParser(allow_no_value=True)
     config.add_section('discord')
     config.add_section('wasd.tv')
     config.add_section('twitch.tv')
     config.set('discord', 'token', '')
-    config.set('discord',
-               '#If True, the bot puts likes under the posts and forms the top of popularity. (default: false)')
-    config.set('discord', 'top_popularity', 'false')
-    config.set('discord',
-               '#If True, custom emoji will be registered (default: false)')
-    config.set('discord', 'custom_like_emojis', 'false')
-    config.set('discord',
-               '#id like emojis')
+    config.set('discord', '#ID like and dislike emojis')
     config.set('discord', 'emoji_like_id', '')
-    config.set('discord',
-               '#id dislike emojis')
     config.set('discord', 'emoji_dislike_id', '')
     config.set('discord',
                '#ID of the channel and the message-top that is stored in this channel')
     config.set('discord', 'channel_top_id', '')
     config.set('discord', 'message_top_id', '')
-    config.set('discord', '#id role for notifications')
+    config.set('discord', '#ID role for notifications')
     config.set('discord', 'notif_role_id', '')
 
     with open(path, 'w') as conf:
         config.write(conf)
-
-
-def getConfig(path):
-    # Returns the config object
-    if not os.path.exists(path):
-        createConfig(path)
-
-    config = configparser.ConfigParser()
-    config.read(path)
     return config
 
 
-def currentTime():
+async def current_time():
     now = datetime.datetime.now()
     return now.strftime("%d-%m-%Y %H:%M:%S")
 
 
-def top_PopularityStr(actualTopID: list, actualTopPoints: list):
-    topTitle = "**Звёзды:**\n"
-    if len(actualTopID) > 9:
-        topList = f"""🥇 <@{actualTopID[0]}> - **{actualTopPoints[0]}**
-                        🥈 <@{actualTopID[1]}> - **{actualTopPoints[1]}**
-                        🥉 <@{actualTopID[2]}> - **{actualTopPoints[2]}**
-                        __4.__   <@{actualTopID[3]}> - **{actualTopPoints[3]}**
-                        __5.__   <@{actualTopID[4]}> - **{actualTopPoints[4]}**
-                        __6.__   <@{actualTopID[5]}> - **{actualTopPoints[5]}**
-                        __7.__   <@{actualTopID[6]}> - **{actualTopPoints[6]}**
-                        __8.__   <@{actualTopID[7]}> - **{actualTopPoints[7]}**
-                        __9.__   <@{actualTopID[8]}> - **{actualTopPoints[8]}**
-                        __10.__ <@{actualTopID[9]}> - **{actualTopPoints[9]}**"""
-        return topTitle + topList
-    topList1, topList2 = "", ""
-    for i in range(len(actualTopID)):
-        topList1 += f"__{i+1}.__   <@{actualTopID[i]}> - **{actualTopPoints[i]}**\n"
-    for i in range(len(actualTopID), 10):
-        topList2 += f"__{i+1}.__\n"
-    return topTitle + topList1 + topList2
+def top_popularity_str(actual_top_id: list, actual_top_points: list):
+    top_title = "**Звёзды:**\n"
+    if len(actual_top_id) > 9:
+        top_list = f"""🥇 <@{actual_top_id[0]}> - **{actual_top_points[0]}**
+                        🥈 <@{actual_top_id[1]}> - **{actual_top_points[1]}**
+                        🥉 <@{actual_top_id[2]}> - **{actual_top_points[2]}**
+                        __4.__   <@{actual_top_id[3]}> - **{actual_top_points[3]}**
+                        __5.__   <@{actual_top_id[4]}> - **{actual_top_points[4]}**
+                        __6.__   <@{actual_top_id[5]}> - **{actual_top_points[5]}**
+                        __7.__   <@{actual_top_id[6]}> - **{actual_top_points[6]}**
+                        __8.__   <@{actual_top_id[7]}> - **{actual_top_points[7]}**
+                        __9.__   <@{actual_top_id[8]}> - **{actual_top_points[8]}**
+                        __10.__ <@{actual_top_id[9]}> - **{actual_top_points[9]}**"""
+        return top_title + top_list
+    top_list1, top_list2 = "", ""
+    for i in range(len(actual_top_id)):
+        top_list1 += f"__{i+1}.__   <@{actual_top_id[i]}> - **{actual_top_points[i]}**\n"
+    for i in range(len(actual_top_id), 10):
+        top_list2 += f"__{i+1}.__\n"
+    return top_title + top_list1 + top_list2
+
+
+async def get_info_database(request: str):
+    global cursor
+    answer = None
+    for row in cursor.execute(request):
+        answer = row[0]
+    return answer
+
+
+async def update_database(author_id: int, author_name: str, value: int):
+    global cursor
+    list_of_ip = []
+    cursor.execute("SELECT id FROM detail")
+    massive_big = cursor.fetchall()
+    for i in range(len(massive_big)):
+        list_of_ip.append(massive_big[i][0])
+
+    if author_id not in list_of_ip:
+        cursor.execute(
+            f"Insert into detail VALUES ('{author_id}', '{author_name}','{0}')")
+        conn.commit()
+
+    for row in cursor.execute(f"Select points FROM detail where id={author_id}"):
+        points_was = row[0]
+    cursor.execute(
+        f"UPDATE detail SET points = {points_was+value} WHERE id = {author_id}")
+    conn.commit()
+    if config['discord'].getboolean('channel_logs'):
+        update_logs(
+            f"[{current_time()}] Update score {author_name} {value} ({points_was}-->{points_was+value})")
+
+
+async def update_logs(message: str):
+    if config['discord'].getboolean('channel_logs'):
+        channel = bot.get_channel(config['discord']['channel_log_id'])
+        await channel.send(message)
 
 
 @bot.event
@@ -98,7 +119,7 @@ async def on_ready():
 async def on_member_remove(member):
     if member.guild.system_channel:
         await member.guild.system_channel.send(
-            f"<@{member.id}> ({member.name}) ушел с сервера! [{currentTime()}]"
+            f"<@{member.id}> ({member.name}) ушел с сервера! [{current_time()}]"
         )
 
 
@@ -132,7 +153,7 @@ async def notifications(ctx):
 
 @bot.command()
 async def f(ctx):
-    author_nick = ctx.author
+    author_name = ctx.author
     await ctx.send(type(ctx))
 
 
@@ -142,189 +163,118 @@ async def on_raw_reaction_add(payload):
     liker_id = payload.user_id  # айди лайкавшего(чтобы не засчитать айди бота)
     if liker_id == bot.user.id:
         return
-    channel = bot.get_channel(payload.channel_id)  # получаем объект канала
+    # получаем объект канала
+    channel = bot.get_channel(payload.channel_id)
     # получаем объект сообщения
     message = await channel.fetch_message(payload.message_id)
-    author_ID = message.author.id  # автор залайконого поста
+    # автор залайконого поста
+    author_id = message.author.id
     # ник автора залайконого поста
-    author_nick = await bot.fetch_user(author_ID)
-    if liker_id == author_ID:
+    if liker_id == author_id:
         return
+    author_name = await bot.fetch_user(author_id)
     liker_nick = await bot.fetch_user(payload.user_id)
-    list_of_ip = []
-    cursor.execute("SELECT id FROM detail")
-    massive_big = cursor.fetchall()
-    for z in range(len(massive_big)):
-        list_of_ip.append(massive_big[z][0])
 
-    if payload.emoji.id == int(config['discord']['emoji_like_id']):  # лайк
-        if author_ID not in list_of_ip:
-            cursor.execute(
-                f"Insert into detail VALUES ('{author_ID}', '{author_nick}','{0}')"
-            )
-            conn.commit()
-        for row in cursor.execute(f"Select points FROM detail where id={author_ID}"):
-            points_was = row[0]
-        cursor.execute(
-            f"UPDATE detail SET points = {points_was+1} WHERE id = {author_ID}"
-        )
-        conn.commit()
-        print(
-            f"[{currentTime()}] {liker_nick} dobavil {author_nick} +1 ({points_was}-->{points_was+1})"
-        )
+    if config['discord'].getboolean('custom_like_emojis'):
+        if payload.emoji.id == int(config['discord']['emoji_like_id']):
+            update_database(author_id, author_name, 1)
+            if config['discord'].getboolean('channel_top'):
+                channel_top = bot.get_channel(
+                    int(config['discord']['channel_top_id']))
 
-        channel_top = bot.get_channel(int(config['discord']['channel_top_id']))
-        try:
-            lstmessage = await channel_top.fetch_message(int(config['discord']['message_top_id']))
-        except:
-            try:
-                lstmessage = await channel_top.fetch_message(
-                    channel_top.last_message_id
-                )
-            except:
-                await channel_top.send(f"Звёзды:")
-            lstmessage = await channel_top.fetch_message(channel_top.last_message_id)
-        actualTopID = []
-        actualTopPoints = []
-        for row in cursor.execute(f"Select id FROM detail ORDER BY points DESC"):
-            actualTopID.append(row[0])
-        for row in cursor.execute(f"Select points FROM detail ORDER BY points DESC"):
-            actualTopPoints.append(row[0])
+                try:
+                    lstmessage = await channel_top.fetch_message(int(config['discord']['message_top_id']))
+                except:
+                    await channel_top.send(f"Звёзды:")
 
-        await lstmessage.edit(content=top_PopularityStr(actualTopID, actualTopPoints))
+                lstmessage = await channel_top.fetch_message(channel_top.last_message_id)
 
-    # дизлайк
-    if payload.emoji.id == int(config['discord']['emoji_dislike_id']):
-        if author_ID not in list_of_ip:
-            cursor.execute(
-                f"Insert into detail VALUES ('{author_ID}', '{author_nick}','{0}')"
-            )
-            conn.commit()
-        for row in cursor.execute(f"Select points FROM detail where id={author_ID}"):
-            points_was = row[0]
-        cursor.execute(
-            f"UPDATE detail SET points = {points_was-1} WHERE id = {author_ID}"
-        )
-        conn.commit()
-        print(
-            f"[{currentTime()}] {liker_nick} dobavil {author_nick} -1 ({points_was}-->{points_was-1})"
-        )
+                actual_top_id = get_info_database(
+                    f'Select id FROM detail ORDER BY points DESC')
+                actual_top_points = get_info_database(
+                    f'Select points FROM detail ORDER BY points DESC')
 
-        channel_top = bot.get_channel(int(config['discord']['channel_top_id']))
-        try:
-            lstmessage = await channel_top.fetch_message(int(config['discord']['message_top_id']))
-        except:
-            try:
-                lstmessage = await channel_top.fetch_message(
-                    channel_top.last_message_id
-                )
-            except:
-                await channel_top.send(f"Звёзды:")
-            lstmessage = await channel_top.fetch_message(channel_top.last_message_id)
-        actualTopID = []
-        actualTopPoints = []
-        for row in cursor.execute(f"Select id FROM detail ORDER BY points DESC"):
-            actualTopID.append(row[0])
-        for row in cursor.execute(f"Select points FROM detail ORDER BY points DESC"):
-            actualTopPoints.append(row[0])
+                await lstmessage.edit(content=top_popularity_str(actual_top_id, actual_top_points))
 
-        await lstmessage.edit(content=top_PopularityStr(actualTopID, actualTopPoints))
+        if payload.emoji.id == int(config['discord']['emoji_dislike_id']):
+            update_database(author_id, author_name, -1)
+            if config['discord'].getboolean('channel_top'):
+                channel_top = bot.get_channel(
+                    int(config['discord']['channel_top_id']))
+
+                try:
+                    lstmessage = await channel_top.fetch_message(int(config['discord']['message_top_id']))
+                except:
+                    await channel_top.send(f"Звёзды:")
+
+                lstmessage = await channel_top.fetch_message(channel_top.last_message_id)
+
+                actual_top_id = get_info_database(
+                    f'Select id FROM detail ORDER BY points DESC')
+                actual_top_points = get_info_database(
+                    f'Select points FROM detail ORDER BY points DESC')
+
+                await lstmessage.edit(content=top_popularity_str(actual_top_id, actual_top_points))
 
 
 @bot.event
 async def on_raw_reaction_remove(payload):
 
-    liker_id = payload.user_id
+    liker_id = payload.user_id  # айди лайкавшего(чтобы не засчитать айди бота)
     if liker_id == bot.user.id:
         return
-
+    # получаем объект канала
     channel = bot.get_channel(payload.channel_id)
+    # получаем объект сообщения
     message = await channel.fetch_message(payload.message_id)
-    author_ID = message.author.id
-    author_nick = await bot.fetch_user(author_ID)
-
-    if liker_id == author_ID:
+    # автор залайконого поста
+    author_id = message.author.id
+    # ник автора залайконого поста
+    if liker_id == author_id:
         return
-
+    author_name = await bot.fetch_user(author_id)
     liker_nick = await bot.fetch_user(payload.user_id)
-    list_of_ip = []
 
-    cursor.execute("SELECT id FROM detail")
-    massive_big = cursor.fetchall()
-    for z in range(len(massive_big)):
-        list_of_ip.append(massive_big[z][0])
+    if config['discord'].getboolean('custom_like_emojis'):
+        if payload.emoji.id == int(config['discord']['emoji_like_id']):
+            update_database(author_id, author_name, -1)
+            if config['discord'].getboolean('channel_top'):
+                channel_top = bot.get_channel(
+                    int(config['discord']['channel_top_id']))
 
-    if payload.emoji.id == int(config['discord']['emoji_like_id']):  # лайк
-        if author_ID not in list_of_ip:
-            cursor.execute(
-                f"Insert into detail VALUES ('{author_ID}', '{author_nick}','{0}')"
-            )
-            conn.commit()
-        for row in cursor.execute(f"Select points FROM detail where id={author_ID}"):
-            points_was = row[0]
-        cursor.execute(
-            f"UPDATE detail SET points = {points_was-1} WHERE id = {author_ID}"
-        )
-        conn.commit()
-        print(
-            f"[{currentTime()}] {liker_nick} ubral {author_nick} +1 ({points_was}-->{points_was-1})"
-        )
+                try:
+                    lstmessage = await channel_top.fetch_message(int(config['discord']['message_top_id']))
+                except:
+                    await channel_top.send(f"Звёзды:")
 
-        channel_top = bot.get_channel(int(config['discord']['channel_top_id']))
-        try:
-            lstmessage = await channel_top.fetch_message(int(config['discord']['message_top_id']))
-        except:
-            try:
-                lstmessage = await channel_top.fetch_message(
-                    channel_top.last_message_id
-                )
-            except:
-                await channel_top.send(f"Звёзды:")
-            lstmessage = await channel_top.fetch_message(channel_top.last_message_id)
-        actualTopID = []
-        actualTopPoints = []
-        for row in cursor.execute(f"Select id FROM detail ORDER BY points DESC"):
-            actualTopID.append(row[0])
-        for row in cursor.execute(f"Select points FROM detail ORDER BY points DESC"):
-            actualTopPoints.append(row[0])
+                lstmessage = await channel_top.fetch_message(channel_top.last_message_id)
 
-        await lstmessage.edit(content=top_PopularityStr(actualTopID, actualTopPoints))
+                actual_top_id = get_info_database(
+                    f'Select id FROM detail ORDER BY points DESC')
+                actual_top_points = get_info_database(
+                    f'Select points FROM detail ORDER BY points DESC')
 
-    if payload.emoji.id == int(config['discord']['emoji_dislike_id']):  # дизлайк
-        if author_ID not in list_of_ip:
-            cursor.execute(
-                f"Insert into detail VALUES ('{author_ID}', '{author_nick}','{0}')"
-            )
-            conn.commit()
-        for row in cursor.execute(f"Select points FROM detail where id={author_ID}"):
-            points_was = row[0]
-        cursor.execute(
-            f"UPDATE detail SET points = {points_was+1} WHERE id = {author_ID}"
-        )
-        conn.commit()
-        print(
-            f"[{currentTime()}] {liker_nick} ubral {author_nick} -1 ({points_was}-->{points_was+1})"
-        )
+                await lstmessage.edit(content=top_popularity_str(actual_top_id, actual_top_points))
 
-        channel_top = bot.get_channel(int(config['discord']['channel_top_id']))
-        try:
-            lstmessage = await channel_top.fetch_message(int(config['discord']['message_top_id']))
-        except:
-            try:
-                lstmessage = await channel_top.fetch_message(
-                    channel_top.last_message_id
-                )
-            except:
-                await channel_top.send(f"Звёзды:")
-            lstmessage = await channel_top.fetch_message(channel_top.last_message_id)
-        actualTopID = []
-        actualTopPoints = []
-        for row in cursor.execute(f"Select id FROM detail ORDER BY points DESC"):
-            actualTopID.append(row[0])
-        for row in cursor.execute(f"Select points FROM detail ORDER BY points DESC"):
-            actualTopPoints.append(row[0])
+        if payload.emoji.id == int(config['discord']['emoji_dislike_id']):
+            update_database(author_id, author_name, 1)
+            if config['discord'].getboolean('channel_top'):
+                channel_top = bot.get_channel(
+                    int(config['discord']['channel_top_id']))
 
-        await lstmessage.edit(content=top_PopularityStr(actualTopID, actualTopPoints))
+                try:
+                    lstmessage = await channel_top.fetch_message(int(config['discord']['message_top_id']))
+                except:
+                    await channel_top.send(f"Звёзды:")
+
+                lstmessage = await channel_top.fetch_message(channel_top.last_message_id)
+
+                actual_top_id = get_info_database(
+                    f'Select id FROM detail ORDER BY points DESC')
+                actual_top_points = get_info_database(
+                    f'Select points FROM detail ORDER BY points DESC')
+
+                await lstmessage.edit(content=top_popularity_str(actual_top_id, actual_top_points))
 
 
 @bot.event
@@ -338,7 +288,12 @@ async def on_message(message):
         await message.add_reaction(emojiLike)
         await message.add_reaction(emojiDislike)
 
-config = getConfig('conf.ini')
+
+try:
+    config = configparser.ConfigParser()
+    config.read('conf.ini')
+except:
+    config = create_config('conf.ini')
 
 
 #token = config['discord']['token']
